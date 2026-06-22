21404013730f60df7e203fd5a8110ccc61b5a59d
flor cordoba <florencicordoba820@gmail.com>
2026-06-22 19:20:28 -0300

    modifique detallecliente

--- vite-project/src/views/DetalleCliente.jsx
+++ vite-project/src/views/DetalleCliente.jsx
@@ -1,5 +1,15 @@
-function DetalleCliente() {
-  return <h1>Detalle Cliente</h1>;
-}
+import { useParams} from "react-router-dom";
+//Agregue el usesParams para obtener el ID del cliente
 
+function DetalleCliente() {
+  const {id} = useParams();
+  
+  return (
+    <div>
+      <h1>Detalle Cliente</h1>;
+      <p>ID del Cliente</p>
+
+    </div>
+  )
+}
 export default DetalleCliente;
