1225652b4e3a858c7c716ecb4fa387086e82dd7f
flor cordoba <florencicordoba820@gmail.com>
2026-06-22 19:57:47 -0300

    obtuve el id y traje los dato del usuario

--- vite-project/src/views/DetalleCliente.jsx
+++ vite-project/src/views/DetalleCliente.jsx
@@ -1,13 +1,27 @@
 import { useParams} from "react-router-dom";
 //Agregue el usesParams para obtener el ID del cliente
+import { useState, useEffect } from "react";
 
 function DetalleCliente() {
   const {id} = useParams();
+  const [cliente, setCliente] = useState(null);
+
+  useEffect(()=>{
+    fetch(`https://fakestoreapi.com./users/${id}`)
+    .then(res => res.json())
+    .then(data => setCliente(data));
+  }, [id]);
+
+  if(!cliente){
+    return <h2>Cargando...</h2>
+  }
   
   return (
     <div>
-      <h1>Detalle Cliente</h1>;
-      <p>Cliente seleccionado: {id}</p>
+      <h1>Detalle Cliente</h1>
+      <p>Nombre: {cliente.name.firstname} {cliente.name.lastname}</p>
+      <p>Email: {cliente.email}</p>
+      <p>Telefono: {cliente.phone}</p>
 
     </div>
   )
