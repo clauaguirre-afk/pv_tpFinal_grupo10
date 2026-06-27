a82541466e7cc8e78c545035e2a106bf151c0bbb
flor cordoba <florencicordoba820@gmail.com>
2026-06-27 20:25:54 -0300

    objeto anidados

--- vite-project/src/views/DetalleCliente.jsx
+++ vite-project/src/views/DetalleCliente.jsx
@@ -7,9 +7,10 @@ function DetalleCliente() {
   const [cliente, setCliente] = useState(null);
 
   useEffect(()=>{
-    fetch(`https://fakestoreapi.com./users/${id}`)
+    fetch(`https://fakestoreapi.com/users/${id}`)
     .then(res => res.json())
-    .then(data => setCliente(data));
+    .then(data => setCliente(data))
+    .catch(error => console.log(error)); //para evitar errores si falla la api
   }, [id]);
 
   if(!cliente){
@@ -19,9 +20,20 @@ function DetalleCliente() {
   return (
     <div>
       <h1>Detalle Cliente</h1>
+
       <p>Nombre: {cliente.name.firstname} {cliente.name.lastname}</p>
       <p>Email: {cliente.email}</p>
       <p>Telefono: {cliente.phone}</p>
+
+      <p>Usuario: {cliente.username}</p>
+      <p>Contraseña: {cliente.password}</p>
+
+      <h3>Dirección</h3>
+
+      <p>Calle: {cliente.address.street}</p>
+      <p>Número: {cliente.address.number}</p>
+      <p>Ciudad: {cliente.address.city}</p>
+      <p>Código Postal: {cliente.address.zipcode}</p>
 
     </div>
   )
