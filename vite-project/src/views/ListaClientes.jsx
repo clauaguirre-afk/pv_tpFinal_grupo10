bfd6be40377d0267e12811fab01fefd5f18d5023
flor cordoba <florencicordoba820@gmail.com>
2026-06-27 21:32:48 -0300

    complete listacliente

--- vite-project/src/views/ListaClientes.jsx
+++ vite-project/src/views/ListaClientes.jsx
@@ -1,5 +1,25 @@
+import {Link} from "react-router-dom"
 function ListaClientes() {
-  return <h1>Lista de Clientes</h1>;
+  const clientes = [
+    {id: 1, nombre: "Cliente 1"},
+    {id: 2, nombre: "Cliente 2"},
+    {id: 3, nombre: "Cliente 3"}
+  ];
+  return (
+    <div>
+      <h1>Lista de Clientes</h1>
+
+    {clientes.map((cliente)=>(
+      <div key={cliente.id}>
+        <p>{cliente.nombre}</p>
+
+        <Link to={`/clientes/${cliente.id}`}>
+        Ver Ficha Completa
+        </Link>
+      </div>
+    ))}
+    </div>
+  );
 }
 
 export default ListaClientes;
