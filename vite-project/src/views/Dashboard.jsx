a01b1be8400da7054b8a3694b898063acc745969
Claudio Aguirre <manurojas147@gmail.com>
2026-06-29 18:02:21 -0300

    mejora en Dashboard

--- vite-project/src/views/Dashboard.jsx
+++ vite-project/src/views/Dashboard.jsx
@@ -1,27 +1,56 @@
 import { useContext } from "react";
-import { useNavigate } from "react-router-dom";
 import { AdminContext } from "../context/AdminContext";
-
-function Dashboard() {
-
-  const { admin, logout } = useContext(AdminContext);
-  const navigate = useNavigate();
-
-  const handleLogout = () => {
-    logout();
-    navigate("/login");
-  };
-
-  return (
-    <div>
-      <h1>Dashboard</h1>
-      <p>Bienvenido {admin?.nombre}</p>
-      <p>Sector: {admin?.sector}</p>
-      <button onClick={handleLogout}>
-        Cerrar sesión
-      </button>
-    </div>
-  );
-}
-
+import {Paper,Typography,Grid,Card,CardContent} from "@mui/material";
+import { useClientesGlobal } from "../context/ClientesContext";
+function Dashboard(){
+  const {admin}=useContext(AdminContext);
+  const {clientes}=useClientesGlobal();
+  return(
+  <>
+    <Typography variant="h4" fontWeight="bold" mb={3}>
+      Dashboard
+    </Typography>
+    <Typography variant="body1" mb={4}> Bienvenido <strong>{admin.nombre}</strong>. 
+      Desde este panel podrá administrar los clientes del sistema.
+    </Typography>
+    <Grid container spacing={3}>
+      <Grid item xs={12} md={4}>
+        <Card>
+          <CardContent>
+            <Typography variant="h6"> 
+              Clientes registrados
+            </Typography>
+            <Typography variant="h3">
+              {clientes.length}
+            </Typography>
+          </CardContent>
+        </Card>
+      </Grid>
+      <Grid item xs={12} md={4}>
+        <Card>
+          <CardContent>
+            <Typography variant="h6">
+              Administrador
+            </Typography>
+            <Typography>
+              {admin.nombre}
+            </Typography>
+          </CardContent>
+        </Card>
+      </Grid>
+      <Grid item xs={12} md={4}>
+        <Card>
+          <CardContent>
+            <Typography variant="h6">
+              Sector
+            </Typography>
+            <Typography>
+              {admin.sector}
+            </Typography>
+          </CardContent>
+        </Card>
+      </Grid>
+    </Grid>
+  </>
+)}
 export default Dashboard;
